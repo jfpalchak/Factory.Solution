6b80e540ce66d003189e8eb0406904c2554e3447
Joey Palchak <joey.palchak@gmail.com>
2023-10-13 08:33:26 -0700

    update Entity properties + create initial Migration

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -5,7 +5,7 @@ namespace Factory.Models
   public class Machine
   {
     public int MachineId { get; set; }
-
+    public string Model { get; set; }
 
     // Collection Navigation Property
     public List<EngineerMachine> JoinEntities { get; }

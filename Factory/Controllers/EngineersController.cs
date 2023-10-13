9e380821071d10f2129110155d8dfcd7e00d50a3
Joey Palchak <joey.palchak@gmail.com>
2023-10-13 08:45:36 -0700

    add Engineers/Index.cshtml + Controller route

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,5 +1,7 @@
 using Factory.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -16,7 +18,8 @@ namespace Factory.Controllers
 
     public ActionResult Index()
     {
-      return View();
+      List<Engineer> model = _db.Engineers.ToList();
+      return View(model);
     }
   }
 }
